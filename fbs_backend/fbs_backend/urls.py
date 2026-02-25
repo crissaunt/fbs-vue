55000d6e6a67f60f466d4c7afb94ead0b174a5e8
Your Name <youremail@example.com>
2026-02-26 01:44:48 +0800

    Restore core Django files deleted in merge

---
+++ fbs_backend/fbs_backend/urls.py
@@ -0,0 +1,46 @@
+"""
+URL configuration for fbs_backend project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('app.urls')),
+    path('flightapp/', include('flightapp.urls')),
+    path('api/', include('fbs_instructor.urls')),
+    
+    #djoser
+    path('api/auth/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls.authtoken')),
+    path('api/token-auth/', obtain_auth_token),
+]
+
+# Serve media files in development
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+# POST /api/auth/users/ (register)
+# POST /api/auth/token/login/ (Login)
+# POST /api/auth/token/logout/ (logout)
+# GET /api/auth/users/me/ (currnt user) builtin
+
+
