91b9980a2eba4c30c90334830aaef9ae38355c78
crissaunt <242018love@gmail.com>
2026-05-12 11:12:22 +0800

    Fix: Set production URL fallbacks for payment redirects

--- fbs_backend/fbs_backend/settings.py
+++ fbs_backend/fbs_backend/settings.py
@@ -261,7 +261,8 @@ SERVER_EMAIL = 'cthmfbs@gmail.com'
 # Support Contact Info (used in emails)
 SUPPORT_EMAIL = 'support@philippineairlines.com'
 SUPPORT_PHONE = '(02) 8855-8888'
-WEBSITE_URL = config('WEBSITE_URL', default='http://localhost:5173/')
+WEBSITE_URL = config('WEBSITE_URL', default='https://fbs-vue.onrender.com/')
+FRONTEND_URL = config('FRONTEND_URL', default='https://fbs-vue.onrender.com/')
 # WEBSITE_URL = 'https://www.philippineairlines.com'
 
 # For development (prints emails to console instead of sending)
